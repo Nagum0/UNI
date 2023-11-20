94200fc7b50ac553b08cb2d93738162ea10c2d2d
zeki <121572632+Nagum0@users.noreply.github.com>
2023-11-20 14:44:52 +0100

    Delete Misc/Nen directory

--- Misc/Nen/dusk.hs
+++
@@ -1,9 +0,0 @@
-module BlackBox where
-
-attackPunch :: (Num a, Show a) => (String, a, a) -> String
-attackPunch ("", _, _) = error "No name!"
-attackPunch (name, health, power) = name ++ " was punched! Health: " ++ show (health - power)
-
-main :: IO ()
-main = do
-    putStrLn (attackPunch ("Chrollo", 100, 20))

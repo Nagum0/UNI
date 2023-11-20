d7b2922328010789de218871ee29093be481c579
Nagum0 <xpteeth@gmail.com>
2023-11-20 15:15:21 +0100

    Test again!

--- FuncProg/Misc/io.hs
+++ FuncProg/Misc/io.hs
@@ -1,11 +1,3 @@
-inc :: Int -> Int
-inc x = x + 1
-
-main :: IO ()
-main = do
-    putStrLn "n: "
-    x <- getLine
-    case reads x of
-        [(n, "")] -> putStrLn $ show (inc n)
-        _         -> putStrLn "Invalid input!"
+testFunc :: Srting
+testFunc = "Test"
 

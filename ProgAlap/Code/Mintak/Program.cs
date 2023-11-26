983cd4b40a051db8ac9a6a76daf39d20e1099e70
Nagum0 <xpteeth@gmail.com>
2023-11-26 23:29:28 +0100

    Programozasi mintak folytatasa.

--- ProgAlap/Code/Mintak/Program.cs
+++ ProgAlap/Code/Mintak/Program.cs
@@ -2,30 +2,41 @@
 
 class Mintak {
 
-    // KERES
-    static (bool van, int ind) Keres(int e, int u, Func<int, bool> T) {
-        bool van = false;
-        int ind = 0;
+    #region METHODS
 
-        for (int i = e; i < u; i++) {
-            if (T(i)) {
-                van = true;
-            }
-            else {
-                ind++;
+    // SZUMMA
+    static int Sum(List<int> xs, Func<int, bool>? T = null) {
+        int sum = 0;
+
+        for (int i = 0; i < xs.Count; i++) {
+            if (T == null || T(i)) {
+                sum += xs[i];
             }
         }
 
-        return (van, ind);
+        return sum;
     }
 
-    // Generis type KERES
-    static (bool van, int ind) GenKeres<T>(List<T> xs, Func<int, bool> F) {
+    // MEGSZAMOLAS - DARAB
+    static int Darab<H>(List<H> xs, Func<int, bool> T) {
+        int db = 0;
+
+        for (int i = 0; i < xs.Count; i++) {
+            if (T(i)) {
+                db++;
+            }
+        }
+
+        return db;
+    }
+
+    // KERES
+    static (bool van, int ind) GenKeres<H>(List<H> xs, Func<int, bool> T) {
         bool van = false;
         int ind = 0;
 
         for (int i = 0; i < xs.Count; i++) {
-            if (F(i)) {
+            if (T(i)) {
                 van = true;
                 break;
             }
@@ -52,27 +63,52 @@ class Mintak {
         }
 
         return (maxind, maxert);
-    }   
+    }
+
+    // KIVALASZTAS
+    static int Kivalaszt<H>(List<H> xs, Func<int, bool> T) {
+        int ind;
+
+        for (ind = 0; ind < xs.Count; ind++) {
+            if (T(ind)) {
+                return ind;
+            }
+        }
+
+        return -1;
+    }
+
+    #endregion
 
     static void Main(string[] args) {
-        #region KERES
-        int e = 0;
-        int u = 5;
-        int[] xs = {1,2,3,-4,5};
-        (bool van, int ind) = Keres(e, u, i => xs[i] < 0);
-        Console.WriteLine(van ? $"Van negativ szam: xs[{ind}]" : "Nincs negativ szam");
+        #region SZUMMA
+        List<int> sum_xs = new List<int> {1,2,3,4,5};
+        int sum = Sum(sum_xs, i => sum_xs[i] % 2 == 0); 
+        Console.WriteLine($"Sum: {sum}");
         #endregion
 
-        #region GenKERES
+        #region DARAB
+        List<string> db_xs = new List<string> {"Astolfo", "Zeki", "Lmao", "Zeki"};
+        int db = Darab(db_xs, i => db_xs[i] == "Zeki");
+        Console.WriteLine($"Zeki was found {db} times.");
+        #endregion
+
+        #region KERES
         List<string> strs = new List<string> {"Astolfo", "Zeki"};
-        (bool van2, int ind2) = GenKeres(strs, i => strs[i] == "Fee");
-        Console.WriteLine(van2 ? $"{van2} : strs[{ind2}] -> {strs[ind2]}" : "Nem talalt!");
+        (bool van, int ind) = GenKeres(strs, i => strs[i] == "Fee");
+        Console.WriteLine(van ? $"{van} : strs[{ind}] -> {strs[ind]}" : "Nem talalt!");
         #endregion
 
         #region MAX
         List<int> xsl = new List<int> {57, 69, 10, 1000};
         (int maxind, int maxert) = Max(xsl, i => i % 2 != 0);
         Console.WriteLine($"xsl[{maxind} : {maxert}]");
+        #endregion
+
+        #region KIVALASZT
+        List<string> kiv_xs = new List<string> {"Astolfo", "Zeki", "Ferris", "Zeki"};
+        int kiv_ind = Kivalaszt(kiv_xs, i => kiv_xs[i] == "Zeki");
+        Console.WriteLine(kiv_ind != -1 ? $"Zeki found at: {kiv_ind}" : "Nem talalt!");
         #endregion
     }
 }

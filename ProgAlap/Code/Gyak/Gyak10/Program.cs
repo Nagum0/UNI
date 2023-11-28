f7c933d7d499fffa0ce0d411ca11319b93e900b9
zeki <xpteeth@gmail.com>
2023-11-28 17:37:45 +0100

    ProgAlap ZH2

---
+++ ProgAlap/Code/Gyak/Gyak10/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Gyak10
+{
+    class Program
+    {
+        static List<H> Filter<H>(List<H> xs, Func<int, bool>? T = null)
+        {
+            List<H> result = new List<H>();
+            
+            for (int i = 0; i < xs.Count; i++)
+            {
+                if (T == null || T(i))
+                {
+                    result.Add(xs[i]);
+                }
+            }
+
+            return result;
+        }
+
+        static List<HOut> Map<HIn, HOut>(List<HIn> xs, Func<HIn, HOut> T)
+        {
+            List<HOut> result = new List<HOut>();
+            
+            for (int i = 0; i < xs.Count; i++)
+            {
+                result.Add(T(xs[i]));
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int> {1,2,3,4,5};
+            List<int> f_xs = Filter(ints, i => ints[i] % 2 == 0);
+            Console.WriteLine(string.Join(", ", f_xs));
+
+            List<int> m_xs = Map(ints, i => i + 1);
+            Console.WriteLine(string.Join(", ", m_xs));
+        }
+    }
+}
---
+++ ProgAlap/Code/Gyak/ProgAlapZH/Program.cs
@@ -0,0 +1,56 @@
+﻿// Bene Zakarias
+// Neptun: UAUYQF
+// A - csoport
+using System;
+using System.Collections.Generic;
+
+namespace ProgAlapZH
+{
+    internal class Program
+    {
+        struct Konyv
+        {
+            public string szerzo;
+            public string cim;
+            public int peldanyszam;
+            public int kikolcs;
+        }
+
+        static List<int> KonyvekSzama(int n, Konyv[] konyvek)
+        {
+            List<int> konyvekSzama = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (konyvek[i].peldanyszam - konyvek[i].kikolcs <= 2)
+                {
+                    konyvekSzama.Add(i + 1);
+                }
+            }
+
+            return konyvekSzama;
+        }
+
+        static void Main(string[] args)
+        {
+            // Beolvasas:
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+            Konyv[] konyvek = new Konyv[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string sor = Console.ReadLine();
+                konyvek[i].szerzo = sor.Split(';')[0];
+                konyvek[i].cim = sor.Split(';')[1];
+                konyvek[i].peldanyszam = int.Parse(sor.Split(';')[2]);
+                konyvek[i].kikolcs = int.Parse(sor.Split(';')[3]);
+            }
+
+            // Feldolgozas:
+            List<int> konyvekSzama = KonyvekSzama(n, konyvek);
+            Console.Write($"{konyvekSzama.Count} ");
+            Console.WriteLine(string.Join(" ", konyvekSzama));
+        }
+    }
+}

ecc82c4eb08088c2e0c6fbf2c58b9e135e137456
zeki <xpteeth@gmail.com>
2023-12-25 16:48:43 +0100

    Finished the code of complex assignmnt

---
+++ ProgAlap/Code/Beadando/bea_komplx_BeneZakarias/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+class IdojarasElorejelzes {
+    static List<List<int>> Beolvas(int n, int m) {
+        List<List<int>> result = new List<List<int>>(); 
+
+        for (int i = 0; i < n; i++) {
+            string[] sor = Console.ReadLine().Split(' ');
+
+            // Uj sor kezdese:
+            result.Add(new List<int>());
+
+            for (int j = 0; j < m; j++) {
+                result[i].Add(int.Parse(sor[j]));
+            }
+        }
+
+        return result;
+    }
+
+    static double Atlag(List<int> telepules) {
+        double result = 0;
+
+        foreach (int i in telepules) {
+            result += i;
+        }
+
+        return result / telepules.Count;
+    }
+
+    static List<int> MelegebbNapokSzama(List<List<int>> telepulesek) {
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < telepulesek.Count; i++) {
+            double atlag = Atlag(telepulesek[i]);
+            int count = 0;
+
+            for (int j = 0; j < telepulesek[i].Count; j++) {
+                if (telepulesek[i][j] > atlag) {
+                    count++;
+                }
+            }
+
+            result.Add(count);
+        }
+
+        return result;
+    }
+
+    static List<int> SorbaAllitas(List<int> eredmenyek) {
+        List<int> result = new List<int>(eredmenyek.Count);
+
+        while (result.Count < eredmenyek.Count) {
+            int maxind = FindMaxIndex(eredmenyek);
+            result.Add(maxind + 1);
+        }
+
+        return result;
+    }
+
+    static int FindMaxIndex(List<int> list) {
+        int maxind = 0;
+        int maxval = list[0];
+
+        for (int i = 0; i < list.Count; i++) {
+            if (list[i] != -1 && list[i] > maxval) {
+                maxval = list[i];
+                maxind = i;
+            }
+        }
+
+        list[maxind] = -1;
+        return maxind;
+    }
+
+    public static void Main(string[] args) {
+        // Beolvasas:
+        int n, m;
+        string[] sor = Console.ReadLine().Split(' ');
+        int.TryParse(sor[0], out n);
+        int.TryParse(sor[1], out m);
+        List<List<int>> telepulesek = Beolvas(n, m);
+
+        // Feldolgozas:
+        List<int> eredmeny = MelegebbNapokSzama(telepulesek);
+        Console.Error.WriteLine($"Eredmenyek: {string.Join(", ", eredmeny)}");
+        List<int> sorban_eredmeny = SorbaAllitas(eredmeny);
+        
+        // Kiiratas:
+        Console.WriteLine(string.Join(" ", sorban_eredmeny));
+    }
+}

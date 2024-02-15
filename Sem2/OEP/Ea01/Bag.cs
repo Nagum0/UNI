17c1623fb97b096e261bf53da7cd827544aa2f94
zeki <xpteeth@gmail.com>
2024-02-15 15:53:49 +0100

    Watched first OEP EA

---
+++ Sem2/OEP/Ea01/Bag.cs
@@ -0,0 +1,38 @@
+﻿namespace Ea01
+{
+    public class Bag
+    {
+        // Fields:
+        private int m;
+        private int max;
+        private List<int> bag = new List<int>();
+
+        // Methods:
+        public Bag(int m)
+        {
+            this.m = m;
+            max = 0;
+            for (int i = 0; i < m; i++) bag.Add(0);
+        }
+
+        public void PutIn(int val)
+        {
+            if (val > m) throw new Exception();
+
+            bag[val]++;
+            if (bag[val] > max) max = val;
+        }
+
+        public int MostFrequent()
+        {
+            if (max <= 0) throw new Exception();
+            return max;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Size: {m}");
+            Console.WriteLine($"Bag: {string.Join(", ", bag)}");
+        }
+    }
+}
---
+++ Sem2/OEP/Ea01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ea01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> data = new List<int> {0,1,4,4,4,5,6,4,4,8};
+            Bag bag = new Bag(data.Count);
+            foreach (int i in data) bag.PutIn(i);
+            bag.Print();
+            Console.WriteLine($"Most frequent value: {bag.MostFrequent()}");
+            
+            try
+            {
+                bag.PutIn(100);
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}

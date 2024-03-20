aa59ca4a3cf0c4ad52edb1f11a0b831358550260
zeki <xpteeth@gmail.com>
2024-03-20 13:44:29 +0100

    OEP Gyak06

---
+++ Sem2/OEP/Gyak06/IntEnumerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+
+namespace Gyak06
+{
+
+    public class IntEnumerator : IEnumerator
+    {
+        private StreamReader inFile;
+        private string path;
+        private Receipt curr;
+        
+        public IntEnumerator(string path)
+        {
+            this.path = path;
+            inFile = new StreamReader(path);
+        }
+
+        public bool MoveNext()
+        {
+            string[] input;
+            Item item;    
+
+            if (!inFile.EndOfStream)
+            {
+                input = Console.ReadLine().Split();
+                curr.cName = input[0];
+
+                for (int i = 1; i < input.Length; i+=2)
+                {
+                    item = new Item();
+                    item.id = int.Parse(input[i]);
+                    item.price = int.Parse(input[i + 1]);
+                    curr.items.Add(item);
+                }
+
+                return true;
+            }
+            else
+                return false; 
+        }
+
+        public void Reset()
+        {
+            inFile.Close();
+            inFile = new StreamReader(path);
+        }
+
+        object IEnumerator.Current
+        {
+            get { return curr; }
+        }
+
+        public Receipt Current
+        {
+            get
+            {
+                try
+                {
+                    return curr;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+
+    public class IntEnum : IEnumerable
+    {
+        private string path;
+
+        public IntEnum(string path)
+        {
+            this.path = path;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator) GetEnumerator();
+        }
+
+        public IntEnumerator GetEnumerator()
+        {
+            return new IntEnumerator(path);
+        }
+    }
+}
---
+++ Sem2/OEP/Gyak06/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Gyak06
+{
+    public struct Receipt
+    {
+        public string cName;
+        public List<Item> items;
+    }
+
+    public struct Item
+    {
+        public int id;
+        public int price;
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Feladat
+            /* IntEnum test = new IntEnum(args[0]);
+            int db_pos = 0;
+            int db_neg = 0;
+            bool neg = false;
+
+            foreach (int i in test)
+            {
+                if (i < 0)
+                    neg = true;
+
+                if (!neg && i % 2 == 0)
+                    db_pos = i;
+                else if (i % 2 == 0)
+                    db_neg++;
+            }
+
+            Console.WriteLine(db_pos);
+            Console.WriteLine(db_neg); */
+
+
+        }
+    }
+}

29c7c27140efef3061067e17cd0cdb1a255fdcaf
Nagum0 <xpteeth@gmail.com>
2024-04-14 21:56:14 +0200

    Started setup on ProgNyelvek Beadando Extended part

---
+++ Sem2/ProgNyelvek/Beadando/walking/game/WalkingBoardWithPlayers.java
@@ -0,0 +1,11 @@
+package walking.game;
+
+public class WalkingBoardWithPlayers extends WalkingBoard {
+    public WalkingBoardWithPlayers(int size) {
+        super(size);
+    }
+
+    public WalkingBoardWithPlayers(int[][] tiles) {
+        super(tiles);
+    }
+}
---
+++ Sem2/ProgNyelvek/Beadando/walking/game/player/Player.java
@@ -0,0 +1,5 @@
+package walking.game.player;
+
+public class Player {
+    
+}

f5eac5475e9561c978e3d337e194a2649aa5dc3f
Nagum0 <xpteeth@gmail.com>
2024-04-18 17:52:16 +0200

    PlayerStructureTest done

--- Sem2/ProgNyelvek/Beadando/walking/game/player/Player.java
+++ Sem2/ProgNyelvek/Beadando/walking/game/player/Player.java
@@ -1,5 +1,24 @@
 package walking.game.player;
 
+import walking.game.util.*;
+
 public class Player {
+    private int score;
+    protected Direction direction = Direction.UP;
     
+    public void addToScore(int score) {
+
+    }
+
+    public void turn() {
+        
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
 }

42f4d601acf0c03cca659e229c619e6231a1e273
Nagum0 <xpteeth@gmail.com>
2024-04-21 10:44:18 +0200

    MadlyRotatingBuccaneer.turn() finished.

--- Sem2/ProgNyelvek/Beadando/Main.java
+++ Sem2/ProgNyelvek/Beadando/Main.java
@@ -3,6 +3,6 @@ import walking.game.*;
 class Main {
     public static void main(String[] args) {
         WalkingBoardWithPlayers wbs = new WalkingBoardWithPlayers(5, 2);
-        wbs.walk(2, 3, 4, 1, 8, 9, 10, 4, 5, 6);
+        wbs.walk(2, 3, 4, 1, 8, 9, 10, 4, 5, 6, 8, 9);
     }
 }
--- Sem2/ProgNyelvek/Beadando/walking/game/player/MadlyRotatingBuccaneer.java
+++ Sem2/ProgNyelvek/Beadando/walking/game/player/MadlyRotatingBuccaneer.java
@@ -1,8 +1,20 @@
 package walking.game.player;
 
 public class MadlyRotatingBuccaneer extends Player {
+    private int turnCount;
+
     @Override
     public void turn() {
+        if (turnCount == 0) {
+            /* System.out.println("Madman: NO TURN"); */
+            turnCount++;
+            return;
+        }
 
+        for (int i = 0; i < turnCount; i++)
+            super.turn();
+
+        /* System.out.println("Madman: " + turnCount + ", " + direction); */
+        turnCount++;
     }
 }

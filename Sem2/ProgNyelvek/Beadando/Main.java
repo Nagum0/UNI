9a5d4c41d43a3896cc640bf75bda7766f3eb5607
Nagum0 <xpteeth@gmail.com>
2024-04-23 13:29:18 +0200

    walk1() test method completed.

--- Sem2/ProgNyelvek/Beadando/Main.java
+++ Sem2/ProgNyelvek/Beadando/Main.java
@@ -2,12 +2,9 @@ import walking.game.*;
 
 class Main {
     public static void main(String[] args) {
-        WalkingBoardWithPlayers wbs = new WalkingBoardWithPlayers(5, 2);
-        int[] ps = wbs.walk(2,3,2,4,2,2,1,2);
-        
+        WalkingBoardWithPlayers wbs = new WalkingBoardWithPlayers(4, 3);
         System.out.println(wbs.toString());
-
-        for (int i : ps)
-            System.out.println(i);
+        int[] ps = wbs.walk(2, 3, 2, 4, 1, 2, 5, 2, 4);
+        System.out.println(wbs.toString());
     }
 }
--- Sem2/ProgNyelvek/Beadando/walking/game/WalkingBoardWithPlayersTest.java
+++ Sem2/ProgNyelvek/Beadando/walking/game/WalkingBoardWithPlayersTest.java
@@ -19,7 +19,26 @@ import walking.game.util.*;
 
 public class WalkingBoardWithPlayersTest {
     @Test
-    public void testMethod() {
-        
+    public void walk1() {
+        int[][] boardEndState = {
+            {3, 1, 1, 1},
+            {3, 3, 3, 4},
+            {3, 3, 3, 5},
+            {6, 6, 6, 5}
+        };
+
+        int[] finalScores = {9, 12, 6};
+
+        WalkingBoardWithPlayers wbs = new WalkingBoardWithPlayers(4, 3);
+        int[] scores = wbs.walk(2, 3, 2, 4, 1, 2, 5, 2, 4);
+        int[][] wbsTiles = wbs.getTiles();
+
+        assertArrayEquals(boardEndState, wbsTiles);
+        assertArrayEquals(finalScores, scores);
+    }
+
+    @Test
+    public void walk2() {
+
     }
 }

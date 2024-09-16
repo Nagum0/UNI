7b16ff9473d66f5f814d0de4b35b8acb04a7fbeb
zeki <xpteeth@gmail.com>
2024-09-16 14:58:57 +0200

    EVA Gyak 02.

---
+++ Sem3/EVA/02Gyak/DocumentStatistics/DocumentStatistics.cs
@@ -0,0 +1,128 @@
+﻿namespace DocumentStatistics
+{
+	internal class DocumentStatistics
+	{
+		private string _filePath;
+		public string FileContent { get; private set; }
+		public Dictionary<string, int> DistinctWordCount { get; private set; }
+		public int CharacterCount { get; private set; }
+		public int NonWhiteSpaceCharacterCount { get; private set; }
+		public int SentenceCount { get; private set; }
+		public int ProperNounCount { get; private set; }
+		public int ColemanLieuIndex { get; private set; }
+		public int FleschReadingEase { get; private set; }
+
+		public DocumentStatistics(string _filePath)
+		{
+			this._filePath = _filePath;
+		}
+
+		// Throws System.IO.IOException
+		public void Load()
+		{
+			FileContent = File.ReadAllText(_filePath);
+			ComputeDistinctWords();
+			CharacterCount = FileContent.Length;
+			int wsChars = FileContent.Count(c => char.IsWhiteSpace(c));
+			NonWhiteSpaceCharacterCount = CharacterCount - wsChars;
+			SentenceCount = ComputeSentenceCount();
+			ProperNounCount = ComputeProperNounCount();
+			ColemanLieuIndex = ComputeColemanLieuIndex();
+			FleschReadingEase = ComputeFleschReadingEase();
+		}
+
+		public void ComputeDistinctWords()
+		{
+			DistinctWordCount = new Dictionary<string, int>();
+			string[] words = FileContent.Split();
+			words = words.Where(s => s.Length > 0).ToArray();
+
+			// Remove the non letter characters from the beginning and
+			// end of the words:
+			for (int i = 0; i < words.Length; i++)
+			{
+				words[i] = string.Concat(
+					words[i]
+						.SkipWhile(c => !char.IsLetter(c))
+						.Reverse()
+						.SkipWhile(c => !char.IsLetter(c))
+						.Reverse()
+					);
+
+				if (string.IsNullOrEmpty(words[i]))
+				{
+					continue;
+				}
+
+				words[i] = words[i].ToLower();
+
+				if (DistinctWordCount.ContainsKey(words[i]))
+				{
+					DistinctWordCount[words[i]]++;
+				}
+				else
+				{
+					DistinctWordCount.Add(words[i], 1);
+				}
+			}
+		}
+
+		private int ComputeSentenceCount()
+		{
+			return FileContent.Count(c => c == '!' || c == '.' || c == '?');
+		}
+
+		private int ComputeProperNounCount()
+		{
+			int count = 0;
+
+			for (int i = 1; i < FileContent.Length; i++)
+			{
+				char c = FileContent[i];
+				char prevC = FileContent[i - 1];
+
+				if (char.IsUpper(c) && prevC != '!' && prevC != '.' && prevC != '?')
+				{
+					count++;
+				}
+			}
+
+			return count;
+		}
+
+		private int ComputeColemanLieuIndex()
+		{
+			int totalWordCount = DistinctWordCount.Sum(p => p.Value);
+			int l = NonWhiteSpaceCharacterCount / totalWordCount * 100;
+			int s = SentenceCount / totalWordCount * 100;
+			return (int)((0.0588 * l) - (0.296 * s) - 15.8);
+		}
+
+		private int ComputeFleschReadingEase()
+		{
+			int syllableCount = FileContent.Split().Sum(word => CountSyllables(word));
+			int totalWordCount = FileContent.Split().Sum(word => word.Length);
+			return (int)(206.835 - 1.015 * (totalWordCount / SentenceCount) - 84.6 * (syllableCount / totalWordCount));
+		}
+
+		private int CountSyllables(string word)
+		{
+			int count = 0;
+
+			for (int i = 0; i < word.Length; i++)
+			{
+				if (IsVowel(word[i]) && i + 1 < word.Length && IsVowel(word[i + 1]))
+				{
+					count++;
+				}
+			}
+
+			return count;
+		}
+
+		private bool IsVowel(char c)
+		{
+			return "aeiouyAEIOUY".IndexOf(c) != -1;
+		}
+	}
+}
---
+++ Sem3/EVA/02Gyak/DocumentStatistics/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace DocumentStatistics
+{
+	internal class Program
+	{
+		static int ReadPositive(string message)
+		{
+			return int.Parse(message);
+		}
+
+		static List<string> ReadIgnoredWords()
+		{
+			return Console.ReadLine().Split(',').ToList();
+		}
+
+		static int Main(string[] args)
+		{
+			// Getting the file path:
+			string? filePath;
+			do
+			{
+				Console.Write("Path: ");
+				filePath = Console.ReadLine();
+			} while (Path.GetExtension(filePath) != ".txt" || !File.Exists(filePath));
+
+			int minOccurrence = 0;
+			do
+			{
+				try
+				{
+					Console.Write("Min occurance: ");
+					string? message = Console.ReadLine();
+					minOccurrence = ReadPositive(message);
+					break;
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+					continue;
+				}
+			} while (true);
+
+			int minLength = 0;
+			do
+			{
+				try
+				{
+					Console.Write("Min length: ");
+					string? message = Console.ReadLine();
+					minLength = ReadPositive(message);
+					break;
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+					continue;
+				}
+			} while (true);
+
+			var ignoredWords = ReadIgnoredWords();
+
+			DocumentStatistics statistics = new(filePath);
+
+			try
+			{
+				statistics.Load();
+				// statistics.ComputeDistinctWords();
+				Console.WriteLine($"Character count: {statistics.CharacterCount}");
+				Console.WriteLine($"Non whitespace character count: {statistics.NonWhiteSpaceCharacterCount}");
+				Console.WriteLine($"Sentence count: {statistics.SentenceCount}");
+				Console.WriteLine($"Proper noun count: {statistics.ProperNounCount}");
+				Console.WriteLine($"Coleman Lieu index: {statistics.ColemanLieuIndex}");
+				Console.WriteLine($"Flesch Reading Ease: {statistics.FleschReadingEase}");
+			}
+			catch (IOException ie)
+			{
+				Console.WriteLine(ie.Message);
+				return -1;
+			}
+
+			var pairs = statistics.DistinctWordCount
+				.Where(p => p.Value >= minOccurrence)
+				.Where(p => p.Key.Length >= minLength)
+				.Where(p => !ignoredWords.Contains(p.Key))
+				.OrderByDescending(p => p.Value);
+			foreach (var pair in pairs)
+			{
+				Console.WriteLine($"{pair.Key} : {pair.Value}");
+			}
+
+			return 0;
+		}
+	}
+}

01c27b8dc3f236470b3d340d288921c715d92804
zeki <xpteeth@gmail.com>
2024-11-07 15:24:18 +0100

    EVA: 08Gyak work.

---
+++ Sem3/EVA/08Gyak/ImageDownloader/Model/WebImage.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http;
+
+namespace ImageDownloader.Model
+{
+    public class WebImage
+    {
+        private byte[] _data;
+        private Uri _url;
+
+        protected WebImage(byte[] data, Uri url)
+        {
+            _data = data;
+            _url = url;
+        }
+
+        public static async Task<WebImage> DownloadAsync(Uri url) 
+        {
+            if (url == null)
+                throw new Exception("URL must be not be null");
+
+            if (!url.IsAbsoluteUri)
+                throw new Exception("URL must be an absolute");
+
+            HttpClient client = new HttpClient();
+            byte[] data = await client.GetByteArrayAsync(url);
+
+            return new WebImage(data, url);
+        }
+
+        public byte[] Data 
+        { 
+            get => _data;
+        }
+
+        public Uri Url 
+        { 
+            get => _url; 
+        }
+    }
+}
---
+++ Sem3/EVA/08Gyak/ImageDownloader/Model/WebPage.cs
@@ -0,0 +1,42 @@
+﻿namespace ImageDownloader.Model
+{
+    public class WebPage
+    {
+        private Uri _baseUrl;
+        private List<WebImage> _images;
+        public event EventHandler<WebImage>? ImageLoaded;
+        public event EventHandler<int>? LoadProgress;
+
+        public WebPage(Uri baseUrl)
+        {
+            if (baseUrl == null)
+                throw new Exception("BaseURL must not be null");
+
+            if (!baseUrl.IsAbsoluteUri)
+                throw new Exception("BaseURL must be absolute");
+
+            _baseUrl = baseUrl;
+            _images = new List<WebImage>();
+        }
+
+        public async Task LoadImageAsync()
+        { 
+            throw new NotImplementedException();
+        }
+
+        public Uri BaseUrl 
+        {
+            get => _baseUrl;
+        }
+
+        public ICollection<WebImage> Images
+        {
+            get => _images;
+        }
+
+        public int ImageCount
+        { 
+            get => _images.Count;
+        }
+    }
+}
